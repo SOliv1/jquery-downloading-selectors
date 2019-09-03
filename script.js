23f425179a5961e65cc824b4a428d08e24324c30
Ubuntu <ubuntu@ip-172-31-95-131.ec2.internal>
2019-09-03 21:29:11 +0000

    turn links yellow

--- script.js
+++ script.js
@@ -24,3 +24,20 @@ $(".stream-nav").on("click", function() {
     $(thisStreamCardsSelector).addClass("card-highlight");
 
 });
+
+
+//wait until page is ready
+
+$(document).ready(function() {
+
+	//sets <a> element within paragraph  to yellow  
+
+	$("p").click(function(){
+
+		$(this).children("a").css("background-color", "yellow"); /* returns all the <a> child elements that are 
+
+		within this paragraph*/
+
+	});
+
+});
