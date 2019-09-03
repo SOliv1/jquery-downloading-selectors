5eb98f2a1a183db54e8ca4a3ebc133e79870c4eb
Ubuntu <ubuntu@ip-172-31-95-131.ec2.internal>
2019-09-03 22:34:52 +0000

    sideways initial commit

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
 
-$(".stream-nav").on("click", function() {
-
-    // A selector to match all cards in all streams
-
-    var allStreamsCardsSelector = ".card";
-
-    // A selector to match just this stream's cards
-
-    // for this, we use the class with the name of the stream,
-
-    // which is the same as this nav link's id and then the "-card" suffix.
-
-    var thisStreamCardsSelector = "." + this.id + "-card";
-
-
-
-    // First remove the highlight from all of the cards
-
-    $(allStreamsCardsSelector).removeClass("card-highlight");
-
-    // Then apply the highlight to just this stream's cards
-
-    $(thisStreamCardsSelector).addClass("card-highlight");
-
-});
-
-
 //wait until page is ready
 
 $(document).ready(function() {
@@ -37,6 +10,16 @@ $(document).ready(function() {
 		$(this).children("a").css("background-color", "yellow"); /* returns all the <a> child elements that are 
 
 		within this paragraph*/
+
+	});
+
+
+
+	// Toggle the visibility of the paragraph when a button is clicked 
+
+	$("button").click(function(){
+
+		$(this).prev().slideToggle('slow');
 
 	});
 
